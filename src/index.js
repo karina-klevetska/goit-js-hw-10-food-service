6e8e9aae593bea757510b9f72a42553362c366c8
Karina Klevetska <karina.klevetska@gmail.com>
2021-08-12 02:11:02 +0300

    add localStorage

--- src/index.js
+++ src/index.js
@@ -14,24 +14,38 @@ const refs = {
 };
 
 const cardMarkup = createFoodCard(cards);
+const currentTheme = localStorage.getItem('theme');
+const parsedCurrentTheme = JSON.parse(currentTheme)
+
 
 refs.themeSwitch.addEventListener('change', changeTheme);
 
+if (parsedCurrentTheme === null) {
+    localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
+} else if (parsedCurrentTheme === Theme.DARK) {
+    refs.body.classList.add(parsedCurrentTheme);
+    refs.themeSwitch.setAttribute('checked', true);
+    localStorage.setItem('theme', JSON.stringify(Theme.DARK));
+} else {
+    refs.body.classList.add(parsedCurrentTheme);
+}
 
-function changeTheme (event) {
+function changeTheme () {
     if (refs.themeSwitch.checked === true) {
         refs.body.classList.add(Theme.DARK)
         refs.body.classList.remove(Theme.LIGHT)
+        refs.themeSwitch.setAttribute('checked', true);
+        localStorage.setItem('theme', JSON.stringify(Theme.DARK));
     } else {
         refs.body.classList.add(Theme.LIGHT);
         refs.body.classList.remove(Theme.DARK)
+        refs.themeSwitch.setAttribute('checked', false);
+        localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
     };
 };
 
 function createFoodCard(cards) {
     return foodCard(cards);
 };
-
-
 
 refs.menu.insertAdjacentHTML('beforeend', cardMarkup);
